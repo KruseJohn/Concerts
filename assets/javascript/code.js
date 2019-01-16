460516320aeb2311ecd6c03839945e83942e1092
Shelby2 <shelbsjb@hotmail.com>
2019-01-16 14:49:46 -0700

    fixed id's and added submit to console useing code.js

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,1 +1,12 @@
 //This is where I would stick my code
+
+$("#submit-Search").on("click" , function(event){
+    event.preventDefault();
+
+    var bandName = $("#Band-Name").val().trim();
+    var city = $("#CityName").val().trim();
+    var dates=$("#Dates").val().trim();
+    var price = $("#PricePay").val().trim();
+
+    console.log(bandName,city,dates,price);
+});
