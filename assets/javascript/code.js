e91f7d82905145731b64795b2c6882dfd8a10f06
dax <43710813+DavidsRepos@users.noreply.github.com>
2019-01-16 20:58:55 -0700

    added JS to pull from Ticketmaster API

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,12 +1,58 @@
-//This is where I would stick my code
+//Submit Search logic
 
-$("#submit-Search").on("click" , function(event){
+bandName = "";
+city = "";
+
+$("#submit-Search").on("click",function(event){
     event.preventDefault();
+    
 
-    var bandName = $("#Band-Name").val().trim();
-    var city = $("#CityName").val().trim();
-    var dates=$("#Dates").val().trim();
-    var price = $("#PricePay").val().trim();
+    bandName = $("#Band-Name").val().trim();
+    city = $("#CityName").val().trim();
+    // var dates=$("#Dates").val().trim();
+    // var price = $("#PricePay").val().trim();
 
-    console.log(bandName,city,dates,price);
+    console.log(bandName,city);
+    ticketInfo();
+    
+    //Function call to Ticketmaster API
+function ticketInfo() {
+ 
+    //Set Variables
+    var TMAPIKEY = "7elxdku9GGG5k8j0Xm8KWdANDgecHMV0";
+    var responseSize = "20";
+    var sorting = "date,desc";
+    var queryURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=" + TMAPIKEY + "&keyword=" + bandName;
+    
+    
+$.ajax({
+   
+    url: queryURL,
+    method: "GET"
+
+    //Process API Get response
+    }).then(function(response){
+        console.log(response);
+        //Loop through for 10 event results print to screen
+        for (i = 0; i < 10; i++) {
+            
+        console.log(response._embedded.events[i].name);
+        console.log(response._embedded.events[i].url);
+        console.log(response._embedded.events[i].dates.start.localDate);
+        console.log(response._embedded.events[i].dates.start.localTime);
+        console.log(response._embedded.events[i].promoter.name);
+        // console.log(response._embedded.events[i].priceRanges[0].currency);
+        console.log(response._embedded.events[i].images[0].url);
+        console.log(response._embedded.events[i]._embedded.venues[0].city.name);
+        console.log(response._embedded.events[i]._embedded.venues[0].country.name);
+        console.log(response._embedded.events[i]._embedded.venues[0].location.longitude);
+        console.log(response._embedded.events[i]._embedded.venues[0].location.latitude);
+        };
+    });
+};
+
 });
+
+
+
+
